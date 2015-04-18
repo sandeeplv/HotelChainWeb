742a0bdf2ed6c1e51ef9b44815088454585d914b
sandeeplv <sandeeplvin@gmail.com>
2015-04-19 01:14:58 +0530

    Adding dirs

---
+++ src/test/java/hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package hello;
+
+/**
+ * Created by sandeep on 4/19/2015.
+ */
+public class HelloWorld {
+    public static void main(String args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
